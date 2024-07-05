3d4b906fdedc2601d8a387577b60e94723fb5412
yitang1 <2039054446@qq.com>
2024-07-05 14:07:32 +0800

    添加恐惧鹦鹉螺的召唤物品
    类似于克隆物品，但是没有像Fargo突变那样的血月期间的限制

---
+++ Content/Items/SummonItems/RedBait.cs
@@ -0,0 +1,51 @@
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using CalamityMod.Items.Materials;
+
+namespace yitangCalamity.Content.Items.SummonItems
+{
+    public class RedBait : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 3;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.rare = ItemRarityID.LightRed;
+            Item.useAnimation = 45;
+            Item.useTime = 45;
+            Item.useStyle = ItemUseStyleID.HoldUp;
+            Item.consumable = false;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+				.AddIngredient(ItemID.Worm)
+				.AddIngredient(ModContent.ItemType<EssenceofHavoc>(), 5)
+				.AddTile(TileID.DemonAltar)
+				.Register();
+        }
+
+        public override bool CanUseItem(Player player) => !NPC.AnyNPCs(NPCID.BloodNautilus) && !Main.dayTime;
+
+        public override bool? UseItem(Player player)
+        {
+            SoundEngine.PlaySound(SoundID.Roar, player.position);
+
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                Vector2 spawnPosition = player.Center - Vector2.UnitY * 600f;
+                NPC.NewNPC(player.GetSource_ItemUse(Item), (int)spawnPosition.X, (int)spawnPosition.Y, NPCID.BloodNautilus);
+            }
+            return true;
+        }
+    }
+}
