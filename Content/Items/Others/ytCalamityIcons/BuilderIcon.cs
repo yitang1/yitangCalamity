324d254f35f4e0baa742ce123dccb993c05342b6
yitang1 <2039054446@qq.com>
2024-07-05 14:36:39 +0800

    整理0.0.0.2版本的汉化

--- Content/Items/Others/ytCalamityIcons/BuilderIcon.cs
+++ Content/Items/Others/ytCalamityIcons/BuilderIcon.cs
@@ -5,9 +5,9 @@ using Terraria.ModLoader;
 
 namespace yitangCalamity.Content.Items.Others.ytCalamityIcons
 {
-    public class StarMerchantIcon : ModItem
+    public class BuilderIcon : ModItem
     {
-        public override string Texture => "yitangCalamity/NPCs/StarMerchant_Head";
+        public override string Texture => "yitangCalamity/NPCs/Builder_Head";
 
         public override void SetDefaults()
         {
