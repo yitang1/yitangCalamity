9668def97b7ec9fe2e7844b0faa041adef856272
yitang1 <2039054446@qq.com>
2024-07-13 17:41:57 +0800

    尝试回调醉仙女的恢复蜡烛的旧版数值
    已经绕晕了，😭😖

--- Content/Buffs/Potions/CalamityHardComb.cs
+++ Content/Buffs/Potions/CalamityHardComb.cs
@@ -49,12 +49,11 @@ namespace yitangCalamity.Content.Buffs.Potions
             calamityPlayer.blueCandle = true;
 			//粉色的活力蜡烛
             calamityPlayer.pinkCandle = true;
-            //紫色的恢复蜡烛
-            /*player.DefenseEffectiveness.Value默认为1f，无法直接更改。
-             DefenseRatioBonus是防御的难度系数，【经典是0.5】/【专家是0.75】/【大师是1】，
+			//紫色的恢复蜡烛
+			/*DefenseEffectiveness.Value是防御的难度系数，【经典是0.5】/【专家是0.75】/【大师是1】，无法直接更改。
              第三个公式意思是在三个难度下，玩家的防御系数相当于是 【0.5+0.15】/【0.75+0.15】/【1+0.15】，
              也就是所谓的物品简介里“增强了玩家15%的防御效果”的意思。*/
-            float original = player.DefenseEffectiveness.Value;
+			float original = player.DefenseEffectiveness.Value;
             float num = original + DefenseRatioBonus;
             player.DefenseEffectiveness *= num / original;
             //黄色的恶念蜡烛Buff在本Mod里有一个克隆版效果(和灾厄的唯一区别就是玩家身上不会有buff图标)
--- Global/GlobalBuffs/ytCalamityGlobalBuffs.cs
+++ Global/GlobalBuffs/ytCalamityGlobalBuffs.cs
@@ -4,6 +4,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using CalamityMod.Buffs.Placeables;
 using yitangCalamity.Content.Buffs.Potions;
+using yitangCalamity.Global.Config;
 
 namespace yitangCalamity.Global.GlobalBuffs
 {
@@ -25,6 +26,35 @@ namespace yitangCalamity.Global.GlobalBuffs
                         Main.npc[m].AddBuff(ModContent.BuffType<CirrusYellowCandleBuff>(), 20, false);
                 }
             }
+
+			if (player.HasBuff<CirrusPurpleCandleBuff>() && ytCalamityConfig.Instance.FuckCalamityAll1)
+			{
+				/*现在这个originalCandle应该是已经拥有蜡烛buff后计算过的难度系数，
+				player.DefenseEffectiveness的值就是classCandleNum或expertCandleNum或masterCandleNum的值。
+				经典是0.5 + 0.1 = 0.6，专家是0.75 + 0.1 = 0.85，大师是1 + 0.1 = 1.1。*/
+				if (!Main.expertMode)
+				{
+					float classCandleNum = 0.6f;
+					float targetNum = 0.65f;
+					float newNum = targetNum / classCandleNum;
+					player.DefenseEffectiveness *= newNum;
+				}
+				else if (Main.expertMode)
+				{
+					float expertCandleNum = 0.85f;
+					float targetNum = 0.9f;
+					float newNum = targetNum / expertCandleNum;
+					player.DefenseEffectiveness *= newNum;
+				}
+				else if (Main.masterMode)
+				{
+					float masterCandleNum = 1.1f;
+					float targetNum = 1.15f;
+					float newNum = targetNum / masterCandleNum;
+					player.DefenseEffectiveness *= newNum;
+				}
+				//最后难度系数应该分别变成经典的0.65、专家的0.9、大师的1.15。
+			}
         }
-    }
+	}
 }
