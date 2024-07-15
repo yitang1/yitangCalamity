0ad98aa43c34a33a81b6fe21eb15a5e1367f3e45
yitang1 <2039054446@qq.com>
2024-07-15 13:04:42 +0800

    修复了新版Fargo突变Mod与本Mod导致的存档无法加载的问题。

--- Content/Items/SummonItems/RainMagic.cs
+++ Content/Items/SummonItems/RainMagic.cs
@@ -7,25 +7,6 @@ using Terraria.Localization;
 
 namespace yitangCalamity.Content.Items.SummonItems
 {
-    public static class MiscMethods
-    {
-        internal static void WriteText(string text)
-        {
-            WriteText(text, new Color(255, 0, 0));
-        }
-
-        internal static void WriteText(string text, Color c)
-        {
-            if (Main.netMode == NetmodeID.Server)
-            {
-                ChatHelper.BroadcastChatMessage(NetworkText.FromLiteral(text), c);
-            }
-            else
-            {
-                Main.NewText(text, c);
-            }
-        }
-    }
     public class RainMagic : ModItem
 	{
         public override void SetStaticDefaults()
@@ -64,20 +45,18 @@ namespace yitangCalamity.Content.Items.SummonItems
 
         private static void RainOff()
         {
-            MiscMethods.WriteText("雨停了！", new Color(0, 128, 255));
+			Main.NewText("雨停了！", new Color(0, 128, 255));
             Main.StopRain();
-            NetMessage.SendData(MessageID.WorldData);
         }
 
         private static void RainOn()
         {
-            MiscMethods.WriteText("开始下雨！", new Color(0, 128, 255));
+			Main.NewText("开始下雨！", new Color(0, 128, 255));
             int day = 86400;
             int hour = day / 24;
             Main.rainTime = hour * 12;
             Main.raining = true;
             Main.maxRaining = Main.cloudAlpha = 0.9f;
-            NetMessage.SendData(MessageID.WorldData);
         }
 
         public override void AddRecipes()
